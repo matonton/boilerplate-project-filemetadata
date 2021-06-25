6e07f0cc7a29c7d1c11af42d5aa772399ad69133
matonton <30802662+matonton@users.noreply.github.com>
2021-06-25 05:37:33 +0000

    added post route for file upload, with analysis of file content

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var cors = require('cors');
+var fileUpload = require('express-fileupload');
 require('dotenv').config()
 
 var app = express();
 
+app.use(fileUpload());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +13,10 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', function(req, res) {
+  var file = req.files.upfile;
+  res.json({ name: file.name, type: file.mimetype, size: file.size });
+});
 
 
 const port = process.env.PORT || 3000;
